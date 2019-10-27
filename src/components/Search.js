b2048e97c0916bf66c400272e275c083859476d4
A-DiRusso <ossurid@gmail.com>
2019-10-27 09:32:43 -0600

    rough draft of a search box

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,18 +3,27 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 
+
+
 export default class Seach extends Component {
-    state = { comic: [] };
+    state = { comic: [], search: null };
 
     async componentDidMount() {
-        const url = `https://xkcd.now.sh/?comic=303`;
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const comic = document.querySelector('#search-term').value;
+        console.log(comic);
+        const url = `https://xkcd.now.sh/?comic=${comic}`;
         const data = await axios.get(url);
         console.log(data.data);
         this.setState({
             comic: data.data
         })
-
     }
+
+
     render() {
         const { img, title: alt, alt: title } = this.state.comic;
 
@@ -36,10 +45,25 @@ export default class Seach extends Component {
                         Search
                     </Link>
                 </li>
-            </ul>
+            </ul>;
+
+
         return <>
             <h1>Search</h1>
             {links}
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    id='search-term'
+                    placeholder='Please enter a number between 1 and 2219'
+                    className="searchInput"
+                />
+                <input
+                    type='submit'
+                    className="searchSubmit btn"
+                    value="Search for a comic"
+                />
+            </form>
+
             <img
                 className="latestImage" 
                 src={img}
